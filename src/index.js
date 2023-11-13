6270cef7c19adb6e126600ae6f87d0115cc0badc
Sarvesh Kulkarni <sskulkarni161@gmail.com>
2023-11-13 16:46:24 +0530

    added apiError & apiResponse utils

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+        throw error
+    })
+})
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`The server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection failed !", error)
+})
